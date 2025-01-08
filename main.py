703d0f8f17bc3968fe92f4aa6e101d273ae85200
asalinao <solofunch@gmail.com>
2025-01-08 17:36:24 +0300

    add_new_model

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 
 from telegramClient.client_methods import start_parsing
 from myBot.bot_start import start_bot
+from database.db import create_database
 
 
 async def main():
@@ -9,4 +10,5 @@ async def main():
 
 
 if __name__ == "__main__":
+    create_database()
     asyncio.run(main())
