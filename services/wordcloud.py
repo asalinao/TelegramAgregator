b0a5478e595a6e478181019738dbd26ba20887fb
asalinao <solofunch@gmail.com>
2025-01-09 11:19:48 +0300

    wordcloud_fix

--- services/wordcloud.py
+++ services/wordcloud.py
@@ -3,24 +3,25 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 from stop_words import get_stop_words
 
-MY_STOP_WORDS = ['http', 'https', 'gmgn', 'gm', 'gn', 'airdrop', 'early', 'wallet', 'claim', 'discord', 'user',
-                 'email', 'season', 'code', 'task', 'submit', 'nft', 'com', 'app', 'token', 'google', 'remind',
-                 'notice', 'status', 'article', 'xyz', 'feed', 'edition', 'www', 'gle', 'forms', 'twitter', 
-                 'emoji', 'otc', 'ceo', 'web3']
 
 def cloud_generate(keywords, filename):
     STOPWORDS_RU = get_stop_words('russian')
     STOPWORDS_EN = get_stop_words('en')
-    STOPWORDS = STOPWORDS_RU + STOPWORDS_EN + MY_STOP_WORDS
+    STOPWORDS = STOPWORDS_RU + STOPWORDS_EN
+
+    keywords = keywords.lower()
+    keywords = keywords.replace('bitcoin', 'btc')
+    keywords = keywords.replace('solana', 'sol')
+    keywords = keywords.replace('ethereum', 'eth')
 
     wordcloud = WordCloud(width = 2000, 
                         height = 1500, 
                         random_state=1, 
                         background_color='black', 
                         margin=20, 
-                        max_words = 20,
+                        max_words = 30,
                         colormap='winter', 
-                        collocations=False,
+                        collocations=True,
                         min_word_length = 3,
                         stopwords = STOPWORDS).generate(keywords)
     
